07c9bd27bc9bf86e0a7bcb1c019d82a21ee4ba6e
Ankur Kumar Singh <aksinghmt@gmail.com>
2016-07-13 16:08:11 +0530

    Added GarmentFactory

---
+++ src/factory_pattern/pkg/GarmentFactory.java
@@ -0,0 +1,16 @@
+package factory_pattern.pkg;
+
+/**
+ * Created by ankurks on 7/13/16.
+ */
+public class GarmentFactory {
+    public Garment GarmentFactory(String type, int periMeter, int length) throws Exception {
+        switch (type) {
+            case "shirt":
+                return new Shirt(periMeter, length);
+            case "trouser":
+                return new Trouser(periMeter, length);
+        }
+        throw new Exception("Strange Garment");
+    }
+}
