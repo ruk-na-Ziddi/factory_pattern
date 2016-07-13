fdc6b24a09f5f0d3ef5ea047604eeada04a82655
Ankur Kumar Singh <aksinghmt@gmail.com>
2016-07-13 15:44:21 +0530

    Added one abstact class called Garments

---
+++ src/factory_pattern/pkg/pkg/Garment.java
@@ -0,0 +1,14 @@
+package factory_pattern.pkg.pkg;
+
+/**
+ * Created by ankurks on 7/13/16.
+ */
+public abstract class Garment {
+    public abstract int getPeriMeter();
+    public abstract int getLenght();
+
+    @Override
+    public String toString(){
+        return "Lenght is: "+this.getLenght()+" and perimeter is :"+this.getPeriMeter();
+    };
+}
